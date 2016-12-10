605f1e5b58021ed292b772303008d579f6b2792c
William Wong <william.wong@wework.com>
2016-12-10 01:31:21 -0500

    Initial commit

---
+++ src/Spectacles.DynamoExporter/Spectacles.DynamoExporter/DynamoExporter.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using Autodesk.DesignScript.Geometry;
+using Spectacles.Net;
+using Spectacles.Net.Data;
+
+namespace Spectacles.DynamoExporter
+{
+  /// <summary>
+  /// A custom implementation of the BaseSpectaclesExporter
+  /// to convert Dynamo geometry to the Spectacles format
+  /// </summary>
+  internal class DynamoExporter : BaseSpectaclesExporter
+  {
+    private Metadata _metadata;
+    private List<SpectaclesGeometry> _spectaclesGeometries;
+    private List<SpectaclesMaterial> _spectaclesMaterials;
+    private SpectaclesObject _spectaclesObject;
+
+
+    private readonly Geometry _dynamoGeometry;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="dynamoGeometry">Dynamo geometry from node input to convert</param>
+    internal DynamoExporter(Geometry dynamoGeometry)
+    {
+      _dynamoGeometry = dynamoGeometry;
+    }
+    
+    /// <summary>
+    /// Provides Metadata object to add additional information
+    /// </summary>
+    /// <param name="metadata">A Metadata instance</param>
+    public override void OnAddMetadata(Metadata metadata)
+    {
+      _metadata = metadata;
+
+      _metadata.generator = "DynamoExporter";
+      _metadata.version   = 1.0;
+      _metadata.type      = "Object";
+      
+      Console.WriteLine(_metadata);
+    }
+
+    /// <summary>
+    /// Provides SpectaclesGeometry list to add geometry to
+    /// </summary>
+    /// <param name="geometries">List of SpectaclesGeometry objects</param>
+    public override void OnAddGeometries(List<SpectaclesGeometry> geometries)
+    {
+      _spectaclesGeometries = geometries;
+
+      // TODO convert Dynamo geometry to Spectacles geometry
+      
+      Console.WriteLine(_dynamoGeometry);
+      Console.WriteLine(_spectaclesGeometries);
+    }
+
+    /// <summary>
+    /// Provides SpectaclesMaterial list to add materials to
+    /// </summary>
+    /// <param name="materials">List of SpectaclesMaterial objects</param>
+    public override void OnAddMaterials(List<SpectaclesMaterial> materials)
+    {
+      _spectaclesMaterials = materials;
+
+      // TODO convert Dynamo materials to Spectacles materials
+      
+      Console.WriteLine(_spectaclesMaterials);
+    }
+
+    /// <summary>
+    /// Provides SpectaclesObject to add scene objects to
+    /// </summary>
+    /// <param name="spectaclesObject">A SpectaclesObject instance</param>
+    public override void OnAddSpectacleObject(SpectaclesObject spectaclesObject)
+    {
+      _spectaclesObject = spectaclesObject;
+
+      // TODO convert Dynamo scenes to Spectacles scenes
+      
+      Console.WriteLine(_spectaclesObject);
+    }
+
+    /// <summary>
+    /// Optional override of base ExportToJson method
+    /// </summary>
+    /// <param name="path">Path to export json file</param>
+    /// <returns>The serialized JSON Spectacles representation</returns>
+    public override string ExportToJson(string path)
+    {
+      Console.WriteLine("Exporting to JSON");
+
+      return base.ExportToJson(path);
+    }
+  }
+}
---
+++ src/Spectacles.DynamoExporter/Spectacles.DynamoExporter/Spectacles.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using Autodesk.DesignScript.Geometry;
+using Autodesk.DesignScript.Runtime;
+
+namespace Spectacles.DynamoExporter
+{
+  /// <summary>
+  /// Nodes for converting Dynamo geometry to Spectacles-compatible JSON output
+  /// </summary>
+  public class Spectacles
+  {
+    /// <summary>
+    /// Private constructor
+    /// </summary>
+    private Spectacles()
+    {
+    }
+
+    /// <summary>
+    /// Launches the Spectacles Viewer in your web browser
+    /// </summary>
+    /// <param name="launch">If true, launch Spectacles</param>
+    public static void Launch(bool launch)
+    {
+      if (launch)
+      {
+        Process.Start("http://tt-acm.github.io/Spectacles.WebViewer/");
+      }
+    }
+
+    /// <summary>
+    /// Compiles Spectacles objects into a JSON representation of a THREE.js scene, 
+    /// which can be opened using the Spectacles viewer.
+    /// </summary>
+    /// <param name="write">Write the Spectacles JSON file to disk?</param>
+    /// <param name="path">Directory to export output</param>
+    /// <param name="fileName">Name of the file</param>
+    /// <param name="geometry">Geometry to input</param>
+    /// <returns>The serialized JSON and a message designating success or failure</returns>
+    [MultiReturn("json", "message")]
+    public static Dictionary<string, string> SceneCompiler(bool write, string path, string fileName, Geometry geometry)
+    {
+      if (!write)
+      {
+        return null;
+      }
+      
+      if (!Directory.Exists(path))
+      {
+        throw new Exception("Path does not exist");
+      }
+
+      var outputPath = Path.Combine(path, fileName);
+
+      var exporter = new DynamoExporter(geometry);
+
+      try
+      {
+        var json = exporter.ExportToJson(outputPath);
+
+        return new Dictionary<string, string>
+        {
+          { "json", json },
+          { "message", $"Spectacles file exported to {outputPath}" }
+        };
+      }
+      catch (Exception)
+      {
+        return new Dictionary<string, string>
+        {
+          { "json", null },
+          { "message", "There was a problem writing the JSON to the file." }
+        };
+      }
+      
+    }
+  }
+}
